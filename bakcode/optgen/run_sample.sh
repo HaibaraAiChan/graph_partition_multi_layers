5cb662952d5c4cc566b86c866c9aba69dafb2fe9
HaibaraAiChan <46984101+HaibaraAiChan@users.noreply.github.com>
2022-10-06 18:32:32 -0700

    Create run_sample.sh

---
+++ bakcode/optgen/run_sample.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+python3 sample.py 0.8 embedding_bag/fbgemm_t856_bs65536_0.pt > output_0.txt
+# python3 sample.py 0.8 embedding_bag/fbgemm_t856_bs65536_1.pt > output_1.txt
+# python3 sample.py 0.8 embedding_bag/fbgemm_t856_bs65536_2.pt > output_2.txt
+# python3 sample.py 0.8 embedding_bag/fbgemm_t856_bs65536_3.pt > output_3.txt
+# python3 sample.py 0.8 embedding_bag/fbgemm_t856_bs65536_4.pt > output_4.txt
+# python3 sample.py 0.8 embedding_bag/fbgemm_t856_bs65536_5.pt > output_5.txt
+# python3 sample.py 0.8 embedding_bag/fbgemm_t856_bs65536_6.pt > output_6.txt
+# python3 sample.py 0.8 embedding_bag/fbgemm_t856_bs65536_7.pt > output_7.txt
+# python3 sample.py 0.8 embedding_bag/fbgemm_t856_bs65536_8.pt > output_8.txt
+# python3 sample.py 0.8 embedding_bag/fbgemm_t856_bs65536_9.pt > output_9.txt
+# python3 sample.py 0.8 embedding_bag/fbgemm_t856_bs65536_10.pt > output_10.txt
+# python3 sample.py 0.8 embedding_bag/fbgemm_t856_bs65536_11.pt > output_11.txt
+# python3 sample.py 0.8 embedding_bag/fbgemm_t856_bs65536_12.pt > output_12.txt
+# python3 sample.py 0.8 embedding_bag/fbgemm_t856_bs65536_13.pt > output_13.txt
+# python3 sample.py 0.8 embedding_bag/fbgemm_t856_bs65536_14.pt > output_14.txt
+# python3 sample.py 0.8 embedding_bag/fbgemm_t856_bs65536_15.pt > output_15.txt
